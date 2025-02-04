35ad1fa3e9a783b31270761b7622f91d5d05df25
dltmdgkr <lseunghak1301@gmail.com>
2025-02-04 16:31:33 +0900

    검색창 토글로 변경

--- client/src/pages/MapPage.tsx
+++ client/src/pages/MapPage.tsx
@@ -3,14 +3,49 @@ import DynamicMap from "../map/DynamicMap";
 import SearchLocation from "../map/SearchLocation";
 import { PlaceType } from "../map/mapTypes";
 import MapMarkerController from "../map/MapMarkerController";
+import { HiOutlinePlusSm } from "react-icons/hi";
+import { IoSearch } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 export default function MapPage() {
   const [places, setPlaces] = useState<PlaceType[]>([]);
   const [selectedPlaceId, setSelectedPlaceId] = useState("");
+  const [toggle, setToggle] = useState(false);
 
   return (
     <div style={{ position: "relative", height: "100vh" }}>
       <DynamicMap>
+        <Link to="/upload">
+          <HiOutlinePlusSm
+            style={{
+              position: "absolute",
+              bottom: "80px",
+              right: "20px",
+              fontSize: "24px",
+              backgroundColor: "white",
+              padding: "10px",
+              borderRadius: "50%",
+              cursor: "pointer",
+              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+              zIndex: 2,
+            }}
+          />
+        </Link>
+        <IoSearch
+          onClick={() => setToggle((prev) => !prev)}
+          style={{
+            position: "absolute",
+            bottom: "20px",
+            right: "20px",
+            fontSize: "24px",
+            backgroundColor: "white",
+            padding: "10px",
+            borderRadius: "50%",
+            cursor: "pointer",
+            boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
+            zIndex: 2,
+          }}
+        />
         <div
           style={{
             position: "absolute",
@@ -26,14 +61,16 @@ export default function MapPage() {
             places={places}
             selectedPlaceId={selectedPlaceId}
           />
-          <SearchLocation
-            onUpdatePlaces={(places) => {
-              setPlaces(places);
-            }}
-            onSelect={(placeId) => {
-              setSelectedPlaceId(placeId);
-            }}
-          />
+          {toggle && (
+            <SearchLocation
+              onUpdatePlaces={(places) => {
+                setPlaces(places);
+              }}
+              onSelect={(placeId) => {
+                setSelectedPlaceId(placeId);
+              }}
+            />
+          )}
         </div>
       </DynamicMap>
     </div>
